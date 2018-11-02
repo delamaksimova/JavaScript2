0eb69417f69c133f679b0dda7eea6e3422008df8
Delgir <delamaksimova@gmail.com>
2018-11-02 17:40:18 +0100

    homework week2 version1

--- Week2/homework/maartjes_work.js
+++ Week2/homework/maartjes_work.js
@@ -45,3 +45,22 @@ const tuesday = [
 const tasks = monday.concat(tuesday);
 
 // Add your code here
+
+// 1. map the tasks to durations in hours
+const hourDurations = tasks.map((x) => x.duration / 60)
+
+// 2. Filter out everything that took less than two hours
+const longDurations = hourDurations.filter((x) => x >= 2)
+
+// 3. Multiply the each duration by a per-hour rate for billing and sum it up
+let hourRate = 7.25
+const totalSalary = longDurations.reduce((sum, x) => {return sum + x * hourRate}, 0)
+
+// 4. Output a formatted Euro amount, rounded to Euro cents
+const euroFormatter = new Intl.NumberFormat('en-SWE', {
+  style: 'currency',
+  currency: 'EUR', 
+  minimumFractionDigits: 2
+})
+
+console.log('For longer tasks the total payment for Maartje is ' + euroFormatter.format(totalSalary))
--- Week2/homework/map_filter.js
+++ Week2/homework/map_filter.js
@@ -2,4 +2,16 @@
 
 const numbers = [1, 2, 3, 4];
 
+const oddNumbers = numbers.filter((x) => x % 2 !== 0)
+const newNumbers = oddNumbers.map((x) => x * 2)
+
+/* const newNumbers = [];
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] % 2 !== 0) {
+    newNumbers.push(numbers[i] * 2);
+  }
+} */
+
+console.log('The doubled numbers are', newNumbers);
 // Add your code here
